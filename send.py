4dc55f95902a8d7f3592964a89255c8009809ea0
Stewart Park <stewartpark92@gmail.com>
2014-11-03 17:38:39 +0900

    Due to the changed strategy.

--- send.py
+++ send.py
@@ -1,23 +1,52 @@
 import urllib2
 import json 
 import sys
+from flask import Flask, request, render_template, redirect
 
-if len(sys.argv) < 2: 
-    print 'Usage: python send.py [msg]'
-    sys.exit()
+# Load the config file.
+_cfg = json.load(file('./config.json'))
+api_key = _cfg['api_key']
+_reg_ids = _cfg['reg_ids']
 
-cfg = json.load(file('./config.json'))
-api_key = cfg['api_key']
-regId = cfg['reg_id']
+def save_config():
+    global _reg_ids
+    _cfg['reg_ids'] = _reg_ids
+    json.dump(_cfg, file('./config.json', 'w'))
 
-data = {
-    'registration_ids': [regId],
-    'data': {
-        'msg': sys.argv[1]
+def send(reg_ids, icon, msg):
+    data = {
+        'registration_ids': reg_ids,
+        'data': {
+            'icon': icon,
+            'msg': msg
+        }
     }
-}
+    request = urllib2.Request('https://android.googleapis.com/gcm/send', data=json.dumps(data), headers={'Authorization': 'key='+api_key, 'Content-Type': 'application/json'})
+    return urllib2.urlopen(request).read()
 
-request = urllib2.Request('https://android.googleapis.com/gcm/send', data=json.dumps(data), headers={'Authorization': 'key='+api_key, 'Content-Type': 'application/json'})
+app = Flask(__name__)
 
-print urllib2.urlopen(request).read()
+@app.route('/')
+def index():
+    return render_template('index.html', reg_ids=_reg_ids)
 
+@app.route('/send')
+def web_send():
+    reg_ids = request.args.get('reg_ids').split(',')
+    icon = request.args.get('icon')
+    msg = request.args.get('msg')
+    send(reg_ids, icon, msg)
+    return redirect('/')
+
+@app.route('/add_reg_id')
+def add_reg_id():
+    reg_id = request.args.get('reg_id')
+    if len(reg_id):
+        _reg_ids.append(reg_id)
+        save_config()
+    return 'ok'
+
+
+if __name__ == '__main__':
+    print 'Go to http://localhost:5000/ to send notifications.'
+    app.run(port=5000)
